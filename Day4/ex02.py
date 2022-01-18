669b5e046990826fc60c1311e361c61e1dfd2c06
skyblue1232 <skybluekmg@gmail.com>
2022-01-18 15:56:04 +0900

    4234

---
+++ Day4/ex02.py
@@ -0,0 +1,11 @@
+name = '고민균'
+print('내 이름은 %s 입니다' % name)
+
+height = 162.25
+print('내 키는 %.2f cm 입니다' % height)
+
+weight = 1000
+print('내 몸무게는 %f kg 입니다' % weight)
+
+year, month, day = 2022,1,18
+print('내 생일은 %d년 %d월 %d일 입니다.' %(year, month, day)) 
